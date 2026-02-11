31b74d2389bf169050b248ce8762c516eecc7210
Pablo <pablobaenav@gmail.com>
2026-02-11 20:22:49 +0000

    Fixed choice menu

--- week4/book_management.py
+++ week4/book_management.py
@@ -33,71 +33,71 @@ menu = """
 choice = int(input(menu))
 
 while choice != 6:
-    if choice == 1:
-        print("Add a new book")
-    elif choice == 2:
-        print("Delete a book")
-    elif choice == 3:
-        print("Update the sales")
-    elif choice == 4:
-        print("Find by author ")
-    elif choice == 5:
-        print("Find the oldest book")
+    # if choice == 1:
+    #     print("Add a new book")
+    # elif choice == 2:
+    #     print("Delete a book")
+    # elif choice == 3:
+    #     print("Update the sales")
+    # elif choice == 4:
+    #     print("Find by author ")
+    # elif choice == 5:
+    #     print("Find the oldest book")
 
-    choice = int(input(menu))
+    # choice = int(input(menu))
     # print(menu)
 
 
 
 # Option 1 - Create
 
-if choice == 1:
-    print("Please, add a new book")
-    title = input("Title:")
+    if choice == 1:
+        print("Please, add a new book")
+        title = input("Title:")
 
-    if title in titles:
-        print("That book is already in the list")
-    else:
-        author = input("Author: \n")
-        year = int(input("Year: \n"))
-        sale = int(input("Tag: \n"))
+        if title in titles:
+            print("That book is already in the list")
+        else:
+            author = input("Author: \n")
+            year = int(input("Year: \n"))
+            sale = int(input("Sale: \n"))
 
-        titles.append(title)
-        authors.append(author)
-        years.append(year)
-        sales.append(sale)
+            titles.append(title)
+            authors.append(author)
+            years.append(year)
+            sales.append(sale)
 
 # Option 2 - Delete
 
-elif choice == 2:
-    print("What book would you delete: \n")
-    title = input("Title to delete: ")
+    elif choice == 2:
+        print("What book would you delete: \n")
+        title = input("Title to delete: ")
 
-    if title in titles:
-        index = titles.index(title)
+        if title in titles:
+            index = titles.index(title)
 
-        del titles[index]
-        del authors[index]
-        del years[index]
-        del sales[index]
-        print("Book deleted")
-    else:
-        print("Book not found!!!")
+            del titles[index]
+            del authors[index]
+            del years[index]
+            del sales[index]
+            print("Book deleted")
+        else:
+            print("Book not found!!!")
 
 
 
 # Option 3 - Update
 
-elif choice == 3:
-    title = input("Title to update: \n")
+    elif choice == 3:
+        title = input("Title to update: \n")
 
-    if title in titles:
-        index = titles.index(title)
-        print(f"{titles[index]} ({years[index]}) by {authors[index]} has sales of {sales[index]}")
+        if title in titles:
+            index = titles.index(title)
+            print(f"{titles[index]} ({years[index]}) by {authors[index]} has sales of {sales[index]}")
 
-        new_sales = int(input("New Sales: "))
-        sales[index] = new_sales
-    else:
-        print("Book not found")
+            new_sales = int(input("New Sales: "))
+            sales[index] = new_sales
+        else:
+            print("Book not found")
 
-        
+    print(menu)
